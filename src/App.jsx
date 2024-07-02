10ee86dee603cbc1c5ff3e807de3a698378896be
Achigyus <70823478+Achigyus@users.noreply.github.com>
2024-07-02 23:15:37 +0100

    do mobile styles

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,14 @@ function App() {
     <div className="container">
       <header className="four_card_feature_header">
         <h1 className="four_card_feature_title">
-          <span>Reliable, efficient delivery</span>Powered by Technology
+          <span>Reliable, efficient delivery</span><br />Powered by Technology
         </h1>
         <p className="four_card_feature_header_title_desc">Our Artificial Intelligence powered tools use millions of project data points
         to ensure that your project is successful</p>
       </header>
       <main className="four_card_feature_main">
         <div className="four_card_feature_card_container">
-          <div className="four_card_feature_card">
+          <div className="four_card_feature_card light_blue">
             <h2 className="four_card_feature_card_title">Supervisor</h2>
             <p className="four_card_feature_card_desc">Monitors activity to identify project roadblocks</p>
             <div className="four_card_feature_card_img_container">
@@ -25,7 +25,7 @@ function App() {
             </div>
           </div>
 
-          <div className="four_card_feature_card">
+          <div className="four_card_feature_card red">
             <h2 className="four_card_feature_card_title">Team Builder</h2>
             <p className="four_card_feature_card_desc">Scans our talent network to create the optimal team for your project</p>
             <div className="four_card_feature_card_img_container">
@@ -33,7 +33,7 @@ function App() {
             </div>
           </div>
 
-          <div className="four_card_feature_card">
+          <div className="four_card_feature_card orange">
             <h2 className="four_card_feature_card_title">Karma</h2>
             <p className="four_card_feature_card_desc">Regularly evaluates our talent to ensure quality</p>
             <div className="four_card_feature_card_img_container">
@@ -41,7 +41,7 @@ function App() {
             </div>
           </div>
 
-          <div className="four_card_feature_card">
+          <div className="four_card_feature_card dark_blue">
             <h2 className="four_card_feature_card_title">Calculator</h2>
             <p className="four_card_feature_card_desc">Uses data from past projects to provide better delivery estimates</p>
             <div className="four_card_feature_card_img_container">
